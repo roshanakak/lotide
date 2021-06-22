a138af0e162696847c43444816066c38ad67ccca
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-22 21:35:19 +0000

    flatten.js added

---
+++ flatten.js
@@ -0,0 +1,40 @@
+const eqArrays = function(firstArray, secondArray) {
+  let output = true;
+  if (firstArray.length !== secondArray.length) {
+    output = output && false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        output = output && false;
+      }
+    }
+  }
+  return output;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  const Pass = String.fromCodePoint(0x1F601);
+  const Fail = String.fromCodePoint(0x1F614);
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`${Pass} Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`${Fail} Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+const flatten = function(elements) {
+  let output = [];
+  for (let element of elements) {
+    if (Array.isArray(element)) {
+      for (let elm of element) {
+        output.push(elm);
+      }
+    } else {
+      output.push(element);
+    }
+  }
+  return output;
+};
+
+//console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
