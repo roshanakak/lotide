cac7d060f997599f7551f0c24c0bea955a0b732e
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-22 20:47:12 +0000

    assertArraysEqual.js modified

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -23,6 +23,6 @@ const assertArraysEqual = function(firstArray, secondArray) {
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]), true;
-assertArraysEqual([1, 2, 3], [3, 2, 1]), false;
-assertArraysEqual([1, 2], [3, 2, 1]), false;
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2], [3, 2, 1]);
