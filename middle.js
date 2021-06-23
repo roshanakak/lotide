8bb44e924285e740c177126c2fe390a197f8a4f2
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-23 16:09:17 +0000

    middle.js added

---
+++ middle.js
@@ -0,0 +1,54 @@
+const assertEqual = function(actual, expected) {
+  const Pass = String.fromCodePoint(0x1F601);
+  const Fail = String.fromCodePoint(0x1F614);
+  if (actual === expected) {
+    console.log(`${Pass}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${Fail}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(firstArray, secondArray) {
+  let output = true;
+  if (firstArray.length !== secondArray.length) {
+    output = output && false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        output = output && false;
+      }
+    }
+  }
+  return output;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  const Pass = String.fromCodePoint(0x1F601);
+  const Fail = String.fromCodePoint(0x1F614);
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`${Pass} Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`${Fail} Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+
+// ACTUAL FUNCTION
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[array.length / 2 - 1], array[array.length / 2]];
+  } else if (array.length % 2 === 1) {
+    return [array[Number.parseInt(array.length / 2)]];
+  }
+};
+
+// TEST CODE
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
