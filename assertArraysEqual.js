4a57653d1fab77fd017c17b311bcd4de8c34482f
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-28 02:36:41 +0000

    assertArrayTest added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(firstArray, secondArray) {
-  let output = true;
-  if (firstArray.length !== secondArray.length) {
-    output = output && false;
-  } else {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {
-        output = output && false;
-      }
-    }
-  }
-  return output;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArray, secondArray) {
   const Pass = String.fromCodePoint(0x1F601);
@@ -22,7 +10,4 @@ const assertArraysEqual = function(firstArray, secondArray) {
   }
 };
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2], [3, 2, 1]);
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const Pass = String.fromCodePoint(0x1F601);
-  const Fail = String.fromCodePoint(0x1F614);
-  if (actual === expected) {
-    console.log(`${Pass}Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${Fail}Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 
 const eqArrays = function(firstArray, secondArray) {
   let output = true;
