e70882e58b7885bf9620e6f65f2a36c7125af199
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-28 02:05:40 +0000

    assertEqual & head refactored for test

--- assertEqual.js
+++ assertEqual.js
@@ -2,14 +2,16 @@ const assertEqual = function(actual, expected) {
   const Pass = String.fromCodePoint(0x1F601);
   const Fail = String.fromCodePoint(0x1F614);
   if (actual === expected) {
-    console.log(`${Pass}Assertion Passed: ${actual} === ${expected}`);
+    console.log(`${Pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${Fail}Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`${Fail} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
+module.exports = assertEqual;
+
 // TEST CODE
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 10);
+// assertEqual("Bootcamp", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 10);
--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  const Pass = String.fromCodePoint(0x1F601);
-  const Fail = String.fromCodePoint(0x1F614);
-  if (actual === expected) {
-    console.log(`${Pass}Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${Fail}Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
