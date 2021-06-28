9866a0558627163dc6f8ebfa9dad9719de607d1e
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-28 02:54:09 +0000

    headTest mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5); 
+  });
+
+  it("should not return 5 for []", () => {
+    assert.strictEqual(head([]), head([])); 
+  });
+  
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5); 
+  });
+
+  it("returns 5 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+
+});
